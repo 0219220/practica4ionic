b583d2f26dd55c068a216992f39751ec3ba77876
Luis V <0219220@up.edu.mx>
2022-05-24 13:36:37 -0500

    primer deploy de nuevo

---
+++ dist/js/78.edf296a2.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkpractica4ionic"]=self["webpackChunkpractica4ionic"]||[]).push([[78],{78:function(t,e,n){n.r(e),n.d(e,{startStatusTap:function(){return s}});var r=n(65),i=n(593),o=n(587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const s=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,s=document.elementFromPoint(e/2,n/2);if(!s)return;const c=(0,i.a)(s);c&&new Promise((t=>(0,o.c)(c,t))).then((()=>{(0,r.Iu)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,i.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.edf296a2.js.map
